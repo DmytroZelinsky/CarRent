d4d7a20be31fad703cd040fc2dafe96ef96adb12
dmytro <zelinsky.dmytro.w@gmail.com>
2021-11-25 03:06:04 +0200

    Add car controller

---
+++ CarRent/Controllers/CarController.cs
@@ -0,0 +1,34 @@
+﻿using CarRent.Models;
+using CarRent.Repositories.Interfaces;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using System.Collections;
+using System.Collections.Generic;
+using System.Threading.Tasks;
+
+namespace CarRent.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class CarController : ControllerBase
+    {
+        private readonly ICarRepo _carRepo;
+
+        public CarController(ICarRepo carRepo)
+        {
+            _carRepo = carRepo;
+        }
+
+        [HttpGet]
+        public async Task<ActionResult<IEnumerable<Car>>> GetAllCars()
+        {
+            return Ok(await _carRepo.GetAllAsync());  
+        }
+
+        [HttpGet("{id}")]
+        public async Task<ActionResult<Car>> GetCarById(int id)
+        {
+            return Ok(await _carRepo.GetFirstAsync(x => x.CarId == id));
+        }
+    }
+}
