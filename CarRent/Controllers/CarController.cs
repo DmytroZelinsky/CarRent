2301354d85ed2f6346f19888672044248b5a56eb
Dmytro <zelinsky.dmytro.w@gmail.com>
2021-11-26 02:18:52 +0200

    Add CRUD operation to car controller

--- CarRent/Controllers/CarController.cs
+++ CarRent/Controllers/CarController.cs
@@ -1,5 +1,9 @@
-﻿using CarRent.Models;
+﻿using AutoMapper;
+using CarRent.DTOs;
+using CarRent.Models;
 using CarRent.Repositories.Interfaces;
+using CarRent.Services.Interfaces;
+using CarRent.ViewModels;
 using Microsoft.AspNetCore.Http;
 using Microsoft.AspNetCore.Mvc;
 using System.Collections;
@@ -12,23 +16,54 @@ namespace CarRent.Controllers
     [ApiController]
     public class CarController : ControllerBase
     {
-        private readonly ICarRepo _carRepo;
-
-        public CarController(ICarRepo carRepo)
+        private readonly ICarService _carService;
+        private readonly IMapper _mapper;
+        public CarController(ICarService carService, IMapper mapper)
         {
-            _carRepo = carRepo;
+            _carService = carService;
+            _mapper = mapper;
         }
 
         [HttpGet]
-        public async Task<ActionResult<IEnumerable<Car>>> GetAllCars()
+        public async Task<ActionResult<IEnumerable<CarView>>> GetAllCars()
         {
-            return Ok(await _carRepo.GetAllAsync());  
+            var cars = await _carService.GetAllCarsAsync();
+            var carsView = _mapper.Map<IEnumerable<CarView>>(cars);
+            return Ok(carsView);
         }
 
-        [HttpGet("{id}")]
-        public async Task<ActionResult<Car>> GetCarById(int id)
+        [HttpGet("{id}", Name = "GetCarById")]
+        public async Task<ActionResult<CarView>> GetCarById(int id)
         {
-            return Ok(await _carRepo.GetFirstAsync(x => x.CarId == id));
+            var car = await _carService.GetCarByIdAsync(id);
+            var carView = _mapper.Map<CarView>(car);
+            return Ok(carView);
         }
+
+        [HttpPost]
+        public async Task<ActionResult<CarView>> CreateCar(CarDTO newCar)
+        {
+            var car = _mapper.Map<Car>(newCar);
+            await _carService.CreateCarAsync(car);
+            var carView = _mapper.Map<CarView>(car);
+            return CreatedAtRoute(nameof(GetCarById), new { Id = carView.CarId }, carView);
+        }
+
+        [HttpPut("{id}")]
+        public async Task<ActionResult> UpdateCar(int id, CarDTO updatedCar)
+        {
+            var car = _mapper.Map<Car>(updatedCar);
+            await _carService.UpdateCarAsync(id, car);
+            var carView = _mapper.Map<CarView>(car);
+            return Ok(carView);
+        }
+
+        [HttpDelete("{id}")]
+        public async Task<ActionResult> DeleteCar(int id)
+        {
+            await _carService.DeleteCarAsync(id);
+            return Ok();
+        }
+
     }
 }
